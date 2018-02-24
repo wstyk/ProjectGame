f9982e01ce75cf0e4627cd8d22bb84eb1a505c70
Maks <maksszaszkow@gmail.com>
2018-02-24 22:46:30 +0100

    Postac

--- Projekt/Assets/Scripts/MainGame/Spells/Earth/EarthWallController.cs
+++ Projekt/Assets/Scripts/MainGame/Spells/Earth/EarthWallController.cs
@@ -3,16 +3,64 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class EarthWallController : MonoBehaviour {
-    float destroy, speed;
+    [HideInInspector]
+    public float destroyTime, speed;
+    float TimePassed, time;
+    [HideInInspector]
+    public EarthSpells scritp;
+    [HideInInspector]
+    public GameObject player;
+    public GameObject lookAt;
+    bool rotateWall, moveWall, truefalse1;
+    Transform StartRotation, EndRotation;
+    Quaternion EndR;
 
 	void Start () {
-        destroy = 4;
+        destroyTime = 4;
+        TimePassed = 0;
+        rotateWall = false;
+        moveWall = false;
         speed = 70;
+        time = 0;
 	}
 
-	void Update () {
-        destroy -= Time.deltaTime;
-        if (destroy <= 0) Destroy(gameObject);
-        if (destroy > 0 && gameObject.transform.position.y < 7) gameObject.transform.position += new Vector3(0, speed * Time.deltaTime, 0);
+	void FixedUpdate () {
+
+        destroyTime -= Time.deltaTime;
+        TimePassed += Time.deltaTime;
+        
+        if (rotateWall) 
+        {
+            Rotate();
+            //time += 0.01f;
+        }
+        if (moveWall) Move();
+        
+        if (destroyTime <= 0) 
+        {
+            scritp.EarthWallCount--;
+            scritp.EarthWalls.Remove(gameObject);
+            Destroy(gameObject);
+        }
+        else if (destroyTime > 0 && gameObject.transform.position.y < 7)
+        {
+            gameObject.transform.position += new Vector3(0, speed * Time.deltaTime, 0);
+        }
 	}
+
+    public void MoveWall()
+    {
+        if(TimePassed >= 2)
+        {
+            
+        }
+    }
+    void Rotate()
+    {
+        
+    }
+    void Move()
+    {
+        
+    }
 }
