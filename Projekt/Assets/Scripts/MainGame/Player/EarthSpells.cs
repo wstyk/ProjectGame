c1e1b7df438a270178ee43269ecc466f0abf4020
Maks <maksszaszkow@gmail.com>
2018-02-25 17:50:57 +0100

    Revert "Postac"
    This reverts commit f9982e01ce75cf0e4627cd8d22bb84eb1a505c70.

--- Projekt/Assets/Scripts/MainGame/Player/EarthSpells.cs
+++ Projekt/Assets/Scripts/MainGame/Player/EarthSpells.cs
@@ -11,11 +11,7 @@ public class EarthSpells : NetworkBehaviour {
     GameObject PlayerCam;
     float EarthWallCD;
     float EarthWallDistance;
-    int Hold;
-    public int EarthWallCount;
-    [SerializeField]
-    public List<GameObject> EarthWalls;
-    public string Off1, Off2, Off3, Deff1, ChosenOff, ChosenDeff;
+    public string Off1, Off2, Off3, Deff1;
 #if UNITY_EDITOR
     [String("Spells prefabs:")]
 #endif
@@ -23,107 +19,42 @@ public class EarthSpells : NetworkBehaviour {
     GameObject earthwallPrefab;
     void Start()
     {
-        ChosenOff = PlayerPrefs.GetString("ChosenOffElement");
-        ChosenDeff = PlayerPrefs.GetString("ChosenDeffElement");
-        EarthWallDistance = 20f;
-        EarthWallCount = 0;
-        Hold = 0;
-        if (ChosenOff == "Earth")
+        EarthWallDistance = 12f;
+        if (PlayerPrefs.GetString("ChosenOffElement") == "Earth")
         {
             Off1 = PlayerPrefs.GetString("Off1");
             Off2 = PlayerPrefs.GetString("Off2");
             Off3 = PlayerPrefs.GetString("Off3");
         }
-        if (ChosenDeff == "Earth")
+        if (PlayerPrefs.GetString("ChosenDeffElement") == "Earth")
         {
             Deff1 = PlayerPrefs.GetString("Deff1");
         }
-        
     }
 	//Wszystkie komentarze odnośnie działania skryptu znajdują się w skrypcie FireSpells;
 	void Update () {
         CmdCooldowns();
-        if (ChosenOff == "Earth")
+        if (Input.GetKeyDown(KeyCode.Mouse1)) 
         {
-            if (Input.GetKeyDown(KeyCode.Mouse0)) CmdSpell(Off1 + "Down");
-            else if (Input.GetKeyUp(KeyCode.Mouse0))
-            {
-                Hold = 0;
-                CmdSpell(Off2 + "Up");
-            }
-            else if (Input.GetKey(KeyCode.Mouse0)) 
-            {
-                Hold++;
-                if(Hold >= 10) CmdSpell(Off1 + "Hold");
-            }
-
-            if (Input.GetKeyDown(KeyCode.Mouse2)) CmdSpell(Off2 + "Down");
-            else if (Input.GetKeyUp(KeyCode.Mouse2))
-            {
-                Hold = 0;
-                CmdSpell(Off2 + "Up");
-            }
-            else if (Input.GetKey(KeyCode.Mouse2))
-            {
-                Hold++;
-                if (Hold >= 10) CmdSpell(Off2 + "Hold");
-            }
-
-            if (Input.GetKeyDown(KeyCode.Mouse3)) CmdSpell(Off3 + "Down");
-            else if (Input.GetKeyUp(KeyCode.Mouse3))
-            {
-                Hold = 0;
-                CmdSpell(Off3 + "Up");
-            }
-            else if (Input.GetKey(KeyCode.Mouse3))
-            {
-                Hold++;
-                if (Hold >= 10) CmdSpell(Off3 + "Hold");
-            }
+            CmdEarthWall(Deff1);
         }
-        if (ChosenDeff == "Earth")
-        {
-            if (Input.GetKeyDown(KeyCode.Mouse1)) CmdSpell(Deff1 + "Down");
-            else if (Input.GetKeyUp(KeyCode.Mouse1)) 
-            {
-                Hold = 0;
-                CmdSpell(Deff1 + "Up");
-            }
-            else if (Input.GetKey(KeyCode.Mouse1))
-            {
-                Hold++;
-                if (Hold >= 30) CmdSpell(Deff1 + "Hold");
-            }
-        }
-    }
+	}
 
     [Command]
-    void CmdSpell(string spell)
+    void CmdEarthWall(string spell)
     {
-        RpcSpell(spell);
+        RpcEarthWall(spell);
     }
 
     //Spelle
     [ClientRpc]
-    void RpcSpell(string spell)
+    void RpcEarthWall(string spell)
     {
         //Ściana ziemii
-        if(spell == "EarthWallDown" && EarthWallCD <=0)
+        if(spell == "EarthWall" && EarthWallCD <=0)
         {
             EarthWallCD = 3f;
-            EarthWallCount++;
-            GameObject EarthWall = Instantiate(earthwallPrefab, gameObject.transform.position + gameObject.transform.forward * EarthWallDistance + new Vector3(0, -10, 0), gameObject.transform.rotation);
-            EarthWall.GetComponent<EarthWallController>().scritp = gameObject.GetComponent<EarthSpells>();
-            EarthWall.GetComponent<EarthWallController>().player = gameObject;
-            EarthWalls.Add(EarthWall);
-        }
-        if(spell == "EarthWallHold" && EarthWallCount > 0)
-        {
-            Hold = 0;
-            foreach(GameObject wall in EarthWalls)
-            {
-                wall.GetComponent<EarthWallController>().MoveWall();
-            }
+            Instantiate(earthwallPrefab, gameObject.transform.position + gameObject.transform.forward * EarthWallDistance + new Vector3(0, -10, 0), gameObject.transform.rotation);
         }
     }
 
--- Projekt/Assets/Scripts/MainGame/Spells/Earth/EarthWallController.cs
+++ Projekt/Assets/Scripts/MainGame/Spells/Earth/EarthWallController.cs
@@ -3,64 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class EarthWallController : MonoBehaviour {
-    [HideInInspector]
-    public float destroyTime, speed;
-    float TimePassed, time;
-    [HideInInspector]
-    public EarthSpells scritp;
-    [HideInInspector]
-    public GameObject player;
-    public GameObject lookAt;
-    bool rotateWall, moveWall, truefalse1;
-    Transform StartRotation, EndRotation;
-    Quaternion EndR;
+    float destroy, speed;
 
 	void Start () {
-        destroyTime = 4;
-        TimePassed = 0;
-        rotateWall = false;
-        moveWall = false;
+        destroy = 4;
         speed = 70;
-        time = 0;
 	}
 
-	void FixedUpdate () {
-
-        destroyTime -= Time.deltaTime;
-        TimePassed += Time.deltaTime;
-        
-        if (rotateWall) 
-        {
-            Rotate();
-            //time += 0.01f;
-        }
-        if (moveWall) Move();
-        
-        if (destroyTime <= 0) 
-        {
-            scritp.EarthWallCount--;
-            scritp.EarthWalls.Remove(gameObject);
-            Destroy(gameObject);
-        }
-        else if (destroyTime > 0 && gameObject.transform.position.y < 7)
-        {
-            gameObject.transform.position += new Vector3(0, speed * Time.deltaTime, 0);
-        }
+	void Update () {
+        destroy -= Time.deltaTime;
+        if (destroy <= 0) Destroy(gameObject);
+        if (destroy > 0 && gameObject.transform.position.y < 7) gameObject.transform.position += new Vector3(0, speed * Time.deltaTime, 0);
 	}
-
-    public void MoveWall()
-    {
-        if(TimePassed >= 2)
-        {
-            
-        }
-    }
-    void Rotate()
-    {
-        
-    }
-    void Move()
-    {
-        
-    }
 }
